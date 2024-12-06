c70c1f944854ddf964139bf593415f01cc9268a5
Caique Franca <caique.franca@aluno.ifsp.edu.br>
2024-12-06 17:11:21 -0300

    Alterações na pagina login

--- Projeto_Hotelaria/js/loginbucha.js
+++ Projeto_Hotelaria/js/loginbucha.js
@@ -1,14 +1,8 @@
 function Login(event) {
-    event.preventDefault(); // Impede o envio do formulário
-
+    event.preventDefault() // impede que o formulário seja enviado assim que o usuário aperte o botão (isso fazia com que a validação não ocorresse)
     var usuario = document.getElementsByName('username')[0].value.toLowerCase();
-    var senha = document.getElementsByName('password')[0].value;  // Pegamos a senha em texto puro
-
-    // Criptografando a senha com SHA-256
-    var senhaCriptografada = CryptoJS.SHA256(senha).toString(CryptoJS.enc.Base64);  // Criptografa a senha
-
-    // Verificando se o usuário e a senha estão corretos
-    if (usuario == "admin" && senhaCriptografada == "d2d2d2a4e9fdd7fd572d26e8d66e9e6d0dfe5d3d7a32a0a5174d9c4456d6b7bd") {  // SHA-256 da senha "admin"
+    var senha = document.getElementsByName('password')[0].value.toLowerCase();
+    if (usuario == "admin" && senha == "admin") {
         localStorage.setItem('logado', 'true');
         window.location.href = 'index.html';
     } else {
