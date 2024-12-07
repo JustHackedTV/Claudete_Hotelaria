d62c9517cd7ab483d508bddc4312deae59e2d312
Caíque <135059792+CaFranca@users.noreply.github.com>
2024-12-07 10:07:33 -0300

    Delete Projeto Hotelaria 26_10_2024 directory

--- Projeto Hotelaria 26_10_2024/js/aluguel.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('form-aluguel').addEventListener('submit', (event) => {
-    event.preventDefault();
-    const hospedeNome = document.getElementById('hospede-aluguel').value;
-    const quartoNumero = document.getElementById('quarto-aluguel').value;
-
-    // Aqui você pode adicionar a lógica para alugar um quarto
-    document.getElementById('mensagem-aluguel').innerText = `Quarto ${quartoNumero} alugado para ${hospedeNome}.`;
-});
