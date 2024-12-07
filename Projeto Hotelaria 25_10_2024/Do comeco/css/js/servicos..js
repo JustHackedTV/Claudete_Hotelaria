a72384e3fb8fff261391c62ea96ddc0f454008dc
Caíque <135059792+CaFranca@users.noreply.github.com>
2024-12-07 10:07:24 -0300

    Delete Projeto Hotelaria 25_10_2024 directory

--- Projeto Hotelaria 25_10_2024/Do comeco/css/js/servicos..js
+++
@@ -1,8 +0,0 @@
-document.getElementById('form-servicos').addEventListener('submit', (event) => {
-    event.preventDefault();
-    const hospedeNome = document.getElementById('hospede-servico').value;
-    const servicoSelecionado = document.getElementById('servico').value;
-
-    // Aqui você pode adicionar a lógica para solicitar um serviço
-    document.getElementById('mensagem-servico').innerText = `Serviço ${servicoSelecionado} solicitado para ${hospedeNome}.`;
-});
